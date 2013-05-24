bf3516bb45f42aad497ab52b9333eb087ff20b18
Danimar Ribeiro <danimaribeiro@gmail.com>
2013-05-23 22:33:18 -0300

    Exemplo bundle 1

---
+++ exemplos/bundle-1/ApresentacaoTdc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ApresentacaoTdc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
